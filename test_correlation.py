477c88c4fc4b404e04db4256e077a27c3480d6bb
dark-tea <31654692+dark-tea@users.noreply.github.com>
2021-11-01 20:00:34 +0800

    Update test_correlation.py

--- test_correlation.py
+++ test_correlation.py
@@ -20,17 +20,19 @@ except:
 
 def run_test(rgb_max = 255):
   
-#     device = torch.device('cuda')
-    input_re_1 = Variable(torch.from_numpy(np.array(np.arange(0,1*2*3*4),np.float32)).resize(1,2,3,4).cuda(),requires_grad=True)
-    input_re_2 = Variable(torch.from_numpy(np.array(np.arange(0,1*2*3*4),np.float32)).resize(1,2,3,4).cuda(),requires_grad=True)
-    input_re_1 = input_re_1.cuda()
-    input_re_2 = input_re_2.cuda()
-    correlation_op = Correlation(3,3,20,1,2)
-    output = correlation_op(input_re_1, input_re_2)
-    output.backward(torch.ones(output.size()).cuda())
-    print('result after bacward')
-    print(input_re_1.grad.data)
-    print(input_re_2.grad.data)
+   device = torch.device('cuda')
+   input_re_1 = Variable(torch.from_numpy(np.array(np.arange(0,1*2*3*4),np.float32)).resize(1,2,3,4).cuda(),requires_grad=True)
+   input_re_2 = Variable(torch.from_numpy(np.array(np.arange(0,1*2*3*4),np.float32)).resize(1,2,3,4).cuda(),requires_grad=True)
+   input_re_1 = input_re_1.cuda()
+   input_re_2 = input_re_2.cuda()
+   correlation_op = Correlation(pad_size=20, kernel_size=1, max_displacement=20, stride1=1, stride2=2,
+                                  corr_multiply=1)
+   output = correlation_op(input_re_1, input_re_2)
+   print(output)
+   output.backward(torch.ones(output.size()).cuda())
+   print('result after bacward')
+   print(input_re_1.grad.data)
+   print(input_re_2.grad.data)
   
 if __name__ == '__main__':
     run_test()
